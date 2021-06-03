7d7c9258f263ac0479e82503e610403b90f568fd
inhtruong <duydat.dhsp@gmail.com>
2021-06-03 14:26:27 +0700

    Update ngày 03/06/21

--- Introduction-to-Java/LinearEquationResolver/src/LinearEquationResolver.java
+++ Introduction-to-Java/LinearEquationResolver/src/LinearEquationResolver.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class LinearEquationResolver {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = input.nextDouble();
+        System.out.print("b: ");
+        double b = input.nextDouble();
+        System.out.print("c: ");
+        double c = input.nextDouble();
+
+        if (a != 0) {
+            double solution = (b - c) / a;
+            System.out.printf("The solution is x: %.2f!", solution);
+        } else {
+            if ( b == 0) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+    }
 }
