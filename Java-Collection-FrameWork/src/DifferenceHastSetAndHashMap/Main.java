0055bb99cd057056729a2fa7902b8e6065f9cb78
inhtruong <duydat.dhsp@gmail.com>
2021-06-29 16:30:39 +0700

    Java Collection Framework

---
+++ Java-Collection-FrameWork/src/DifferenceHastSetAndHashMap/Main.java
@@ -0,0 +1,36 @@
+package DifferenceHastSetAndHashMap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Nam",20, "HN");
+        Student student2 = new Student("Hung",21, "HN");
+        Student student3 = new Student("Ha",22, "HN");
+
+        Map<Integer, Student> studentMap = new HashMap<>(4);
+        studentMap.put(1,student1);
+        studentMap.put(2,student2);
+        studentMap.put(3,student3);
+        studentMap.put(4,student1);
+
+        for (Map.Entry<Integer, Student> student : studentMap.entrySet()) {
+            System.out.println(student.toString());
+        }
+
+        Set<Student> studentSet = new HashSet<>();
+        studentSet.add(student1);
+        studentSet.add(student2);
+        studentSet.add(student3);
+        studentSet.add(student1);
+
+        for(Student student : studentSet){
+            System.out.println(student.toString());
+        }
+
+
+    }
+}
---
+++ Java-Collection-FrameWork/src/DifferenceHastSetAndHashMap/Student.java
@@ -0,0 +1,50 @@
+package DifferenceHastSetAndHashMap;
+
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student () {
+
+    }
+
+    public Student (String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
