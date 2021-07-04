329632e494b5fac6eb3296b63507adaa25bb92e5
inhtruong <duydat.dhsp@gmail.com>
2021-07-04 23:54:04 +0700

    Testing And TDD ver.2

--- Automated-Test-And-TDD/Test/TriangleClassifier/TriangleClassifierTest.java
+++ Automated-Test-And-TDD/Test/TriangleClassifier/TriangleClassifierTest.java
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleClassifierTest {
-
     @Test
+    void triangle() {
+        assertEquals("tam giác đều",TriangleClassifier.Triangle(2,2,2));
+        assertEquals("tam giác cân",TriangleClassifier.Triangle(2,2,3));
+        assertEquals("tam giác thường",TriangleClassifier.Triangle(3,4,5));
+        assertEquals("không phải là tam giác",TriangleClassifier.Triangle(8,2,3));
+        assertEquals("không phải là tam giác",TriangleClassifier.Triangle(-1,2,1));
+        assertEquals("không phải là tam giác",TriangleClassifier.Triangle(0,1,1));
+
+    }
 }
---
+++ Automated-Test-And-TDD/src/TriangleClassifier/TriangleClassifier.java
@@ -0,0 +1,18 @@
+package TriangleClassifier;
+
+public class TriangleClassifier {
+    public static String Triangle(int a, int b, int c){
+
+        if(a + b > c && b + c > a && a + c > b){
+            if(a == b && b == c){
+                return  "tam giác đều";
+            }else if(a == b || a == c){
+                return  "tam giác cân";
+            }else{
+                return  "tam giác thường";
+            }
+        }else {
+            return "không phải là tam giác";
+        }
+    }
+}
